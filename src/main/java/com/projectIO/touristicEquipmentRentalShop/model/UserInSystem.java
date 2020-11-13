49bb07ad16cc2fed57f72dd0065190e0522dbecc
Patryk Fidrych <perevistp@gmail.com>
2020-11-14 00:04:58 +0100

    Add new field in UserInSystem class - UserType

---
+++ src/main/java/com/projectIO/touristicEquipmentRentalShop/model/UserInSystem.java
@@ -0,0 +1,35 @@
+package com.projectIO.touristicEquipmentRentalShop.model;
+
+public class UserInSystem {
+    private String login;
+    private UserType userType;
+
+    private static UserInSystem instance;
+
+    public static UserInSystem getInstance() {
+        if (instance == null)
+            return new UserInSystem();
+        else
+            return instance;
+    }
+
+    private UserInSystem() {
+        userType = UserType.GUEST;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public  void setLogin(String login) {
+        this.login = login;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+}
